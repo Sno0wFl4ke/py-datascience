5d65607d7ce9a38fadbb0b6309e6c4044a431ba3
lucastoltenberg <lucastoltenberg2004@gmail.com>
2025-09-09 12:34:57 +0200

    plot show and began with N clouds

--- src/kmeans.py
+++ src/kmeans.py
@@ -9,7 +9,7 @@ def generate_data(my=np.array([0.0, 0.0]), covariance=np.array([[1.0, 0.0], [0.0
     size = n  # int or tuple of ints, optional
     check_valid = 'warn'  # 'warn', 'raise', or 'ignore', optional
     tol = 1e-8  # float, optional
-
+    #print(np.random.multivariate_normal(mean, cov, size=size, check_valid=check_valid, tol=tol))
     return np.random.multivariate_normal(mean, cov, size=size, check_valid=check_valid, tol=tol)
 
 
@@ -22,6 +22,15 @@ def is_diagonal_psd (matrix: np.ndarray) -> bool:
 def draw():
     plt.scatter(generate_data())
     plt.show()
+    
+def N_clouds(N: int, n:int, my=np.array([0.0, 0.0]), covariance=np.array([[1.0, 0.0], [0.0, 1.0]])):
+    all_points = []
+    for i in range(N):
+        all_points.append(generate_data(my,covariance, n))
+    
 
 if __name__ == "__main__":
-    draw()
+    array_points = N_clouds
+    for i in array_points:
+        plt.scatter(i[0],i[1])
+    plt.show()
