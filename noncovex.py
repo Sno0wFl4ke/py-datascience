cf812f9bac662af3185cffcba80bd4d247f65a6a
lucastoltenberg <lucastoltenberg2004@gmail.com>
2025-09-10 18:10:07 +0200

    noncnvex update

--- noncovex.py
+++ noncovex.py
@@ -23,27 +23,27 @@ def eucledean_distances(x: np.array):
             matrix[i, j] = np.linalg.norm(x[i] - x[j])
 
 
-def adjacency_matrix_knn(D: np.ndarray,i:int, k: int):
+def adjacency_matrix_knn(D: np.ndarray, k: int):
+    
     n = D.shape[0]
     A = np.zeros((n, n), dtype=int)
     
     for i in range(n):
         distances = D[i]
         neighbors = np.argsort(distances)
-        neighbors = neighbors[neighbors != i]  
+        neighbors = neighbors[neighbors != i]  # sich selbst ausschließen
         neighbors = neighbors[:k]
+        A[i, neighbors] = 1
         
-        A[i, neighbors] = 1  
-    
     return A
 
-def adjacency_matrix_radius(D: np.ndarray,i: int, r: float):
+def adjacency_matrix_radius(D: np.ndarray, r: float):
     
     n = D.shape[0]
     A = np.zeros((n, n), dtype=int)
     
     for i in range(n):
-        neighbors = np.where((D[i] <= r) & (D[i] > 0))[0] 
+        neighbors = np.where((D[i] <= r) & (D[i] > 0))[0]  # sich selbst ausschließen
         A[i, neighbors] = 1
     
     return A
@@ -55,10 +55,40 @@ def laplacian(W: np.ndarray) -> np.ndarray:
     L = D - W
     return L
 
-rad = adjacency_matrix_radius(worth,0,10)
-knn = adjacency_matrix_knn(worth,0,8)
+rad = adjacency_matrix_radius(worth,10)
+knn = adjacency_matrix_knn(worth,8)
 
-print(laplacian(rad))
+Lrad = laplacian(rad)
+Lknn = laplacian(knn)
+
+eigvals_knn, eigvecs_knn = np.linalg.eig(Lrad)
+eigvals_rad, eigvecs_rad = np.linalg.eig(Lknn)
+
+for i in eigvals_knn:
+    if i < 0:
+        print("Negative eigenvalue in knn:", i)
+
+for i in eigvals_rad:
+    if i < 0:
+        print("Negative eigenvalue in radius:", i)
+
+M = 4
+
+
+idx_knn = np.argsort(eigvals_knn)
+Y_knn = eigvecs_knn[:, idx_knn[:M]]  # N x M
+
+
+idx_rbf = np.argsort(eigvals_rad)
+Y_rbf = eigvecs_rad[:, idx_rbf[:M]]
+
+
+k = 2
+centerpoints_knn, centroids_knn = km.k_means(k, Y_knn)
+centerpoints_rbf, centroids_rbf = km.k_means(k, Y_rbf)
+plt.scatter(Y_knn[:, 0], Y_knn[:, 1],c=centerpoints_knn, cmap='tab10', s=50)
+plt.scatter(Y_rbf[:, 0], Y_rbf[:, 1],c=centerpoints_rbf, cmap='tab10', s=50)
+plt.show()
 
 centerpoints, y = km.k_means(2, worth)
 plt.scatter(worth[:, 0],worth[:, 1], c=centerpoints, cmap='tab10', s=50)
